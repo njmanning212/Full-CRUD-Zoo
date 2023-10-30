daad728f8035c76a2eba1d00ab6a4184e6b5b4f5
Nicholas Manning <njmanning212@gmail.com>
2023-10-30 17:32:26 -0500

    create Animal model

---
+++ Models/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace Full_CRUD_Zoo;
+
+public class Animal
+{
+  public int AnimalID { get; set; }
+  public string Name { get; set; }
+  public string Species { get; set; }
+  public int Age {get; set;}
+  public string Diet { get; set; }
+  public string Photo { get; set; }
+  [DisplayName("Date Of Birth")]
+  public DateTime DateOfBirth { get; set; }
+  [DisplayName("Date Aquired")]
+  public DateTime DateAquired { get; set; }
+  [DisplayName("Last Fed")]
+  public DateTime LastFed { get; set; }
+
+}
