35f2c7c5b90922615c1b93f32c289c8673c944f8
Nicholas Manning <njmanning212@gmail.com>
2023-10-31 15:42:03 -0500

    update delete animal get/post logic, include logger for deletion errors and error message to communicate to user

--- Pages/Animals/Delete.cshtml.cs
+++ Pages/Animals/Delete.cshtml.cs
@@ -7,30 +7,41 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using FullCRUDZoo.Data;
 using Full_CRUD_Zoo;
+using System.Runtime;
 
 namespace FullCRUDZoo.Pages.Animals
 {
     public class DeleteModel : PageModel
     {
         private readonly FullCRUDZoo.Data.ZooContext _context;
+        private readonly ILogger<DeleteModel> _logger;
 
-        public DeleteModel(FullCRUDZoo.Data.ZooContext context)
+        public DeleteModel(FullCRUDZoo.Data.ZooContext context, ILogger<DeleteModel> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         [BindProperty]
         public Animal Animal { get; set; } = default!;
-        public string? Age { get; set; }
+        public string Age { get; set; }
+        public string ErrorMessage { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync(int? id, bool? saveChangesError = false)
         {
             if (id == null || _context.Animals == null)
             {
                 return NotFound();
             }
 
-            Animal = await _context.Animals.FindAsync(id);
+            Animal = await _context.Animals
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.AnimalID == id);
+            
+            if (Animal == null)
+            {
+                return NotFound();
+            }
 
             DateTime Today = DateTime.Now;
 
@@ -43,9 +54,9 @@ namespace FullCRUDZoo.Pages.Animals
                 Age = (Today.Year - Animal.DateOfBirth.Year).ToString() + " years old";
             }
 
-            if (Animal == null)
+            if (saveChangesError.GetValueOrDefault())
             {
-                return NotFound();
+                ErrorMessage = $"Delete {Animal.Name} failed. Try again.";
             }
 
             return Page();
@@ -53,20 +64,29 @@ namespace FullCRUDZoo.Pages.Animals
 
         public async Task<IActionResult> OnPostAsync(int? id)
         {
-            if (id == null || _context.Animals == null)
+            if (id == null)
             {
                 return NotFound();
             }
+
             var animal = await _context.Animals.FindAsync(id);
 
-            if (animal != null)
+            if (animal == null)
             {
-                Animal = animal;
-                _context.Animals.Remove(Animal);
-                await _context.SaveChangesAsync();
+                return NotFound();
             }
 
-            return RedirectToPage("./Index");
+            try 
+            {
+                _context.Animals.Remove(animal);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            catch (DbUpdateException ex)
+            {
+                _logger.LogError(ex, "Error deleting animal");
+                return RedirectToAction("./Delete", new { id, saveChangesError = true });
+            }
         }
     }
 }
